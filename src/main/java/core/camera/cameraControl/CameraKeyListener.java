b2602cfa71981fb074cb11788d962394349ab7a2
kornilov-mr <kornilov.mr@gmail.com>
2024-06-19 19:03:59 +0200

    fixed camera movement delay

--- src/main/java/core/camera/cameraControl/CameraKeyListener.java
+++ src/main/java/core/camera/cameraControl/CameraKeyListener.java
@@ -3,9 +3,13 @@ package core.camera.cameraControl;
 import core.render.RenderConfig;
 import core.camera.Camera;
 
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
 
 public class CameraKeyListener implements KeyListener, Updatable {
@@ -20,6 +24,35 @@ public class CameraKeyListener implements KeyListener, Updatable {
     public CameraKeyListener(Camera camera, RenderConfig renderConfig) {
         this.camera = camera;
         this.renderConfig = renderConfig;
+        new Timer(20, new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                if(!keyPressedSet.isEmpty()){
+                    for(int key : keyPressedSet){
+                        switch (key){
+                            case KeyEvent.VK_W:
+                                dx+=0.1;
+                                break;
+                            case KeyEvent.VK_S:
+                                dx-=0.1;
+                                break;
+                            case KeyEvent.VK_A:
+                                dy-=0.1;
+                                break;
+                            case KeyEvent.VK_D:
+                                dy+=0.1;
+                                break;
+                            case KeyEvent.VK_SPACE:
+                                dz-=0.1;
+                                break;
+                            case KeyEvent.VK_SHIFT:
+                                dz+=0.1;
+                                break;
+                        }
+                    }
+                }
+            }
+        }).start();
     }
 
     @Override
@@ -29,28 +62,6 @@ public class CameraKeyListener implements KeyListener, Updatable {
     @Override
     public void keyPressed(KeyEvent e) {
         keyPressedSet.add(e.getKeyCode());
-        for(int key : keyPressedSet){
-            switch (key){
-                case KeyEvent.VK_W:
-                    dx+=0.1;
-                    break;
-                case KeyEvent.VK_S:
-                    dx-=0.1;
-                    break;
-                case KeyEvent.VK_A:
-                    dy-=0.1;
-                    break;
-                case KeyEvent.VK_D:
-                    dy+=0.1;
-                    break;
-                case KeyEvent.VK_SPACE:
-                    dz-=0.1;
-                    break;
-                case KeyEvent.VK_SHIFT:
-                    dz+=0.1;
-                    break;
-            }
-        }
     }
     @Override
     public void keyReleased(KeyEvent e) {
